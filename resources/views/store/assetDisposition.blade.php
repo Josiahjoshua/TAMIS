7c6c394f77788a417ac2dfa0da79d0007220f6c9
Katonestory <107171649+Katonestory@users.noreply.github.com>
2023-09-08 10:50:47 +0300

    link store buttons

--- resources/views/store/assetDisposition.blade.php
+++ resources/views/store/assetDisposition.blade.php
@@ -8,7 +8,7 @@
                 <div class="breadcome-list shadow-reset">
                     <div class="row">
                         <div class="col-lg-6">
-                            <button class="btn btn-custon-rounded-three btn-success" data-toggle="modal" data-target="#InformationproModalalert"><i class="fa fa-plus"></i> Employee</button>
+                            <button class="btn btn-custon-rounded-three btn-success" data-toggle="modal" data-target="#InformationproModalalert"><i class="fa fa-plus"></i> Dispose Asset</button>
                              <div id="InformationproModalalert" class="modal modal-adminpro-general fullwidth-popup-InformationproModal zoomInUp" role="dialog">
                     <div class="modal-dialog">
                         <div class="modal-content">
@@ -22,7 +22,7 @@
                 <div class="sparkline9-list shadow-reset">
                     <div class="sparkline9-hd">
                         <div class="main-sparkline9-hd">
-                            <h1>User Information Encoding <span class="basic-ds-n">Form</span></h1>
+                            <h1>Asset Disposal Information <span class="basic-ds-n">Form</span></h1>
 
                         </div>
                     </div>
@@ -31,21 +31,21 @@
                             <div class="row">
                                 <div class="col-lg-12">
                                     <div class="basic-login-inner">
-                                        <h3>Fill In : </h3>
+
                                             <div class="form-group-inner">
                                                 <div class="row">
                                                     <div class="col-lg-4">
-                                                        <label class="login2">Full Name : </label>
+                                                        <label class="login2">Asset Code: </label>
                                                     </div>
                                                     <div class="col-lg-8">
-                                                        <input type="text" class="form-control" placeholder="Enter Employee Full Name" />
+                                                        <input type="text" class="form-control" placeholder="Enter Asset Code" />
                                                     </div>
                                                 </div>
                                             </div>
                                             <div class="form-group-inner">
                                                 <div class="row">
                                                     <div class="col-lg-4">
-                                                        <label class="login2">Username : </label>
+                                                        <label class="login2">Asset Description: </label>
                                                     </div>
                                                     <div class="col-lg-8">
                                                         <input type="text" class="form-control" placeholder="Enter Username" />
@@ -54,25 +54,25 @@
                                             </div>
                                             <div class="form-group-inner">
                                             <div id="pwd-container3">
-                                                <div class="form-group">
+                                                {{-- <div class="form-group">
                                                     <input type="password" class="form-control example3" id="password3" placeholder="Password">
-                                                </div>
+                                                </div> --}}
                                                 <div class="form-group">
                                                     <div class="pwstrength_viewport_progress2"></div>
                                                 </div>
                                             </div>
                                             </div>
                                             <div class="form-group-inner">
-                                                <div class="row">
+                                                {{-- <div class="row">
                                                     <div class="col-lg-4">
                                                         <label class="login2">Address : </label>
                                                     </div>
                                                     <div class="col-lg-8">
                                                         <input type="text" class="form-control" placeholder="Enter Address" />
                                                     </div>
-                                                </div>
+                                                </div> --}}
                                             </div>
-                                            <div class="form-group-inner">
+                                            {{-- <div class="form-group-inner">
                                                 <div class="row">
                                                     <div class="col-lg-4">
                                                         <label class="login2">Contact : </label>
@@ -81,7 +81,7 @@
                                                         <input type="number" class="form-control" placeholder="Enter Contact Number" />
                                                     </div>
                                                 </div>
-                                            </div>
+                                            </div> --}}
                                     </div>
                                 </div>
                             </div>
@@ -93,21 +93,21 @@
             </div>
                  <div class="modal-footer footer-modal-admin">
                                 <a data-dismiss="modal" href="#">Cancel</a>
-                                <a href="#">Save</a>
+                                <a href="#">Dispose</a>
                             </div>
                         </form>
                         </div>
                     </div>
                 </div>
                         </div>
-                        <div class="col-lg-6">
+                        {{-- <div class="col-lg-6">
                             <ul class="breadcome-menu">
                                 <li><a href="#">Dashboard</a> <span class="bread-slash">/</span>
                                 </li>
                                 <li><span class="bread-blod">Employee Information</span>
                                 </li>
                             </ul>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
             </div>
@@ -123,7 +123,7 @@
                 <div class="sparkline13-list shadow-reset">
                     <div class="sparkline13-hd">
                         <div class="main-sparkline13-hd">
-                            <h1>User Information <span class="table-project-n"> Encoding  </span> Form Table</h1>
+                            <h1>Asset <span class="table-project-n"> Disposition</span> Form Table</h1>
                             <div class="sparkline13-outline-icon">
                                 <span class="sparkline13-collapse-link"><i class="fa fa-chevron-up"></i></span>
                                 <span><i class="fa fa-wrench"></i></span>
@@ -137,11 +137,10 @@
                             <table id="table" data-toggle="table" data-pagination="true" data-search="true" data-show-columns="true"  data-show-refresh="true" data-key-events="true"  data-resizable="true" data-cookie="true" data-cookie-id-table="saveId"  data-click-to-select="true" data-toolbar="#toolbar">
                                 <thead>
                                     <tr>
-                                        <th>Employee Name</th>
-                                        <th >Username</th>
-                                        <th >Password</th>
-                                        <th >Address</th>
-                                        <th>Contact</th>
+                                        <th>Asset Code</th>
+                                        <th >Asset Description</th>
+                                        <th >Condition</th>
+                                        <th >Status</th>
                                         <th ></th>
                                     </tr>
                                 </thead>
@@ -151,7 +150,6 @@
                                         <td>Jkiller11</td>
                                         <td>**********</td>
                                         <td >445 Mount Eden Road, Mount Eden, Auckland</td>
-                                        <td>+6393333556</td>
                                         <td><button type="button" class="btn btn-primary btn-circle" data-toggle="modal" data-target="#edit"><i class="fa fa-edit"></i></button> <button type="button" class="btn btn-danger"><i class="fa fa-trash"></i></button></td>
                                     </tr>
                                     <tr>
@@ -159,7 +157,6 @@
                                         <td>Jkiller11</td>
                                         <td>*****************</td>
                                         <td >445 Mount Eden Road, Mount Eden, Auckland</td>
-                                        <td>+6393333556</td>
                                         <td><button type="button" class="btn btn-primary btn-circle" data-toggle="modal" data-target="#edit"><i class="fa fa-edit"></i></button> <button type="button" class="btn btn-danger"><i class="fa fa-trash"></i></button></td>
                                     </tr>
                                 </tbody>
--- resources/views/store/assetInformation.blade.php
+++ resources/views/store/assetInformation.blade.php
@@ -7,7 +7,7 @@
                 <div class="breadcome-list shadow-reset">
                     <div class="row">
                         <div class="col-lg-6">
-                            <button class="btn btn-custon-rounded-three btn-success" data-toggle="modal" data-target="#InformationproModalalert"><i class="fa fa-plus"></i> Employee</button>
+                            <button class="btn btn-custon-rounded-three btn-success" data-toggle="modal" data-target="#InformationproModalalert"><i class="fa fa-plus"></i>Assign Asset</button>
                              <div id="InformationproModalalert" class="modal modal-adminpro-general fullwidth-popup-InformationproModal zoomInUp" role="dialog">
                     <div class="modal-dialog">
                         <div class="modal-content">
@@ -21,7 +21,7 @@
                 <div class="sparkline9-list shadow-reset">
                     <div class="sparkline9-hd">
                         <div class="main-sparkline9-hd">
-                            <h1>User Information Encoding <span class="basic-ds-n">Form</span></h1>
+                            <h1>Asset Assignment  <span class="basic-ds-n">Form</span></h1>
 
                         </div>
                     </div>
@@ -30,57 +30,73 @@
                             <div class="row">
                                 <div class="col-lg-12">
                                     <div class="basic-login-inner">
-                                        <h3>Fill In : </h3>
-                                            <div class="form-group-inner">
-                                                <div class="row">
-                                                    <div class="col-lg-4">
-                                                        <label class="login2">Full Name : </label>
-                                                    </div>
-                                                    <div class="col-lg-8">
-                                                        <input type="text" class="form-control" placeholder="Enter Employee Full Name" />
-                                                    </div>
+
+                                        <div class="form-group-inner">
+                                            <div class="row">
+                                                <div class="col-lg-4">
+                                                    <label class="login2">Condition : </label>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <input type="text" class="form-control" placeholder="Enter Condition" style="width: 140%" />
+
+                                                        <input type="number" class="form-control" placeholder="Qty" style="margin-left: 150%; margin-top: -28%; width: 75%">
                                                 </div>
                                             </div>
-                                            <div class="form-group-inner">
-                                                <div class="row">
-                                                    <div class="col-lg-4">
-                                                        <label class="login2">Username : </label>
-                                                    </div>
-                                                    <div class="col-lg-8">
-                                                        <input type="text" class="form-control" placeholder="Enter Username" />
-                                                    </div>
+                                        </div>
+                                        <div class="form-group-inner">
+                                            <div class="row">
+                                                <div class="col-lg-4">
+                                                    <label class="login2">Date Assigned : </label>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <input type="date" class="form-control" placeholder="Enter Date" style="width: 121%" />
+
+                                                        <input type="number" class="form-control" placeholder="Remarks" style="margin-left: 130%; margin-top: -28%; width: 95%">
                                                 </div>
                                             </div>
-                                            <div class="form-group-inner">
-                                            <div id="pwd-container3">
-                                                <div class="form-group">
-                                                    <input type="password" class="form-control example3" id="password3" placeholder="Password">
+                                        </div>
+                                        <div class="form-group-inner">
+                                            <div class="row">
+                                                <div class="col-lg-4">
+                                                    <label class="login2">Item : </label>
                                                 </div>
-                                                <div class="form-group">
-                                                    <div class="pwstrength_viewport_progress2"></div>
-                                                </div>
-                                            </div>
-                                            </div>
-                                            <div class="form-group-inner">
-                                                <div class="row">
-                                                    <div class="col-lg-4">
-                                                        <label class="login2">Address : </label>
-                                                    </div>
-                                                    <div class="col-lg-8">
-                                                        <input type="text" class="form-control" placeholder="Enter Address" />
-                                                    </div>
+                                                <div class="col-lg-8">
+                                                <select class="form-control">
+                                <option value="">Select Name</option>
+                                <option value="all">Item 1</option>
+                                <option value="selected">Item 2</option>
+                                                </select>
                                                 </div>
                                             </div>
-                                            <div class="form-group-inner">
-                                                <div class="row">
-                                                    <div class="col-lg-4">
-                                                        <label class="login2">Contact : </label>
-                                                    </div>
-                                                    <div class="col-lg-8">
-                                                        <input type="number" class="form-control" placeholder="Enter Contact Number" />
-                                                    </div>
+                                        </div>
+                                        <div class="form-group-inner">
+                                            <div class="row">
+                                                <div class="col-lg-4">
+                                                    <label class="login2">Employee : </label>
+                                                </div>
+                                                <div class="col-lg-8">
+                                                <select class="form-control">
+                                <option value="">Select Name</option>
+                                <option value="all">Name 1</option>
+                                <option value="selected">Name 2</option>
+                                                </select>
                                                 </div>
                                             </div>
+                                        </div>
+                                        <div class="form-group-inner">
+                                            <div class="row">
+                                                <div class="col-lg-4">
+                                                    <label class="login2">Branch : </label>
+                                                </div>
+                                                <div class="col-lg-8">
+                                                <select class="form-control">
+                                <option value="">Select Location</option>
+                                <option value="all">Branch 1</option>
+                                <option value="selected">Branch 2</option>
+                                                </select>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -101,9 +117,9 @@
                         </div>
                         <div class="col-lg-6">
                             <ul class="breadcome-menu">
-                                <li><a href="#">Dashboard</a> <span class="bread-slash">/</span>
+                                {{-- <li><a href="#">Dashboard</a> <span class="bread-slash">/</span> --}}
                                 </li>
-                                <li><span class="bread-blod">Employee Information</span>
+                                {{-- <li><span class="bread-blod">Employee Information</span> --}}
                                 </li>
                             </ul>
                         </div>
@@ -122,7 +138,7 @@
                 <div class="sparkline13-list shadow-reset">
                     <div class="sparkline13-hd">
                         <div class="main-sparkline13-hd">
-                            <h1>User Information <span class="table-project-n"> Encoding  </span> Form Table</h1>
+                            <h1>Asset Information <span class="table-project-n">   </span> Form Table</h1>
                             <div class="sparkline13-outline-icon">
                                 <span class="sparkline13-collapse-link"><i class="fa fa-chevron-up"></i></span>
                                 <span><i class="fa fa-wrench"></i></span>
@@ -136,21 +152,21 @@
                             <table id="table" data-toggle="table" data-pagination="true" data-search="true" data-show-columns="true"  data-show-refresh="true" data-key-events="true"  data-resizable="true" data-cookie="true" data-cookie-id-table="saveId"  data-click-to-select="true" data-toolbar="#toolbar">
                                 <thead>
                                     <tr>
-                                        <th>Employee Name</th>
-                                        <th >Username</th>
-                                        <th >Password</th>
-                                        <th >Address</th>
+                                        <th>Asset Code</th>
+                                        <th >Asset description</th>
+                                        <th >Assigned To</th>
+                                        <th >department</th>
                                         <th>Contact</th>
                                         <th ></th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
-                                        <td>Java Advance</td>
-                                        <td>Jkiller11</td>
-                                        <td>**********</td>
-                                        <td >445 Mount Eden Road, Mount Eden, Auckland</td>
-                                        <td>+6393333556</td>
+                                        <td>TIRDO 07889</td>
+                                        <td>Table</td>
+                                        <td>Elibariki Basso</td>
+                                        <td >ICT Department</td>
+                                        <td>0747564873</td>
                                         <td><button type="button" class="btn btn-primary btn-circle" data-toggle="modal" data-target="#edit"><i class="fa fa-edit"></i></button> <button type="button" class="btn btn-danger"><i class="fa fa-trash"></i></button></td>
                                     </tr>
                                     <tr>
